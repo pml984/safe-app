b503542895e25e47f22ba471d8cc4e125c54c5f7
jpkeyw <jpolirer@keywcorp.com>
2016-04-28 16:46:52 +0000

    Complete uploads screen

---
+++ test/modules/create-source.spec.js
@@ -0,0 +1,193 @@
+/* globals afterEach, describe, it */
+
+import {apiUri} from '../../config.js'
+import configureStore from 'redux-mock-store'
+import expect from 'expect'
+import nock from 'nock'
+import {REQUEST as SOURCE_REQUEST} from '../../src/js/modules/sources'
+import thunk from 'redux-thunk'
+import {
+  createSource,
+  createSourceFailure,
+  createSourceRequest,
+  createSourceSuccess,
+  FAILURE,
+  default as reducer,
+  REQUEST,
+  SUCCESS
+} from '../../src/js/modules/create-source'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('createSource actions', () => {
+  describe('sync actions', () => {
+    it('createSourceFailure should create a FAILURE action', () => {
+      const error = new Error('test error')
+      const expectedAction = {
+        payload: {error},
+        type: FAILURE
+      }
+
+      expect(createSourceFailure(error)).toEqual(expectedAction)
+    })
+
+    it('createSourceRequest should create a REQUEST action', () => {
+      const expectedAction = {
+        type: REQUEST
+      }
+
+      expect(createSourceRequest()).toEqual(expectedAction)
+    })
+
+    it('createSourceSuccess should create a SUCCESS action', () => {
+      const expectedAction = {
+        payload: {data: {}},
+        recievedAt: null,
+        type: SUCCESS
+      }
+      const action = createSourceSuccess({})
+
+      expectedAction.recievedAt = action.recievedAt
+
+      expect(action).toEqual(expectedAction)
+    })
+  })
+
+  describe('async actions', () => {
+    afterEach(() => {
+      nock.cleanAll()
+    })
+
+    it('createSource creates a SUCCESS action on success', (done) => {
+      nock(apiUri)
+        .post('/source')
+        .reply(200, {_id: 'abc123', name: 'Name'})
+
+      const name = 'Name'
+      const requestAction = {
+        type: REQUEST
+      }
+      const receiveAction = {
+        type: SUCCESS,
+        payload: {
+          data: {_id: 'abc123', name}
+        },
+        recievedAt: null
+      }
+      const sourceRequestAction = {
+        type: SOURCE_REQUEST
+      }
+      const store = mockStore({})
+
+      store.dispatch(createSource(name))
+        .then(() => {
+          const actions = store.getActions()
+          const expectedActions = [
+            requestAction,
+            receiveAction,
+            sourceRequestAction
+          ]
+
+          expectedActions[1].recievedAt = actions[1].recievedAt
+
+          // call slice because we need to weed out the request that comes from another module
+
+          expect(actions).toEqual(expectedActions)
+          done()
+        })
+    })
+
+    it('createSource creates a FAILURE action on failure', (done) => {
+      nock(apiUri)
+        .get('/source')
+        .reply(500)
+
+      const name = 'Name'
+      const error = new Error('NetworkError')
+      const requestAction = {
+        type: REQUEST
+      }
+      const failureAction = {
+        payload: {error},
+        type: FAILURE
+      }
+      const store = mockStore({})
+
+      store.dispatch(createSource(name))
+        .then(() => {
+          const actions = store.getActions()
+          const expectedActions = [
+            requestAction,
+            failureAction
+          ]
+
+          expectedActions[1].payload.error = actions[1].payload.error
+
+          expect(actions).toEqual(expectedActions)
+          done()
+        })
+    })
+  })
+})
+
+describe('createSource reducer', () => {
+  it('should return the initial state', () => {
+    const stateAfter = {
+      data: {},
+      error: undefined,
+      isFetching: false,
+      lastUpdated: null
+    }
+
+    expect(reducer(undefined, {})).toEqual(stateAfter)
+  })
+
+  it('should handle FAILURE', () => {
+    const error = new Error('test error')
+    const action = {
+      payload: {error},
+      type: FAILURE
+    }
+    const stateAfter = {
+      data: {},
+      error,
+      isFetching: false,
+      lastUpdated: null
+    }
+
+    expect(reducer(undefined, action)).toEqual(stateAfter)
+  })
+
+  it('should handle REQUEST', () => {
+    const action = {
+      type: REQUEST
+    }
+    const stateAfter = {
+      data: {},
+      error: undefined,
+      isFetching: true,
+      lastUpdated: null
+    }
+
+    expect(reducer(undefined, action)).toEqual(stateAfter)
+  })
+
+  it('should handle SUCCESS', () => {
+    const data = {_id: 'abc123', name: 'Name'}
+    const action = {
+      payload: {data},
+      recievedAt: Date.now(),
+      type: SUCCESS
+    }
+    const result = reducer(undefined, action)
+    const expectedValue = {
+      data,
+      error: undefined,
+      isFetching: false,
+      lastUpdated: result.lastUpdated
+    }
+
+    expect(result).toEqual(expectedValue)
+  })
+})
