b010a2f230a6ac6272e181a6d42f070015b5768f
Kristine Norris <kris.norris@vistronix.com>
2016-04-29 19:04:49 +0000

    Closes #79. Added bar-chart tests

--- test/modules/bar-chart.spec.js
+++ test/modules/bar-chart.spec.js
@@ -10,18 +10,37 @@ import {
 describe('barChart actions', () => {
   it('setBarChart should create a SET action', () => {
     const barChart = {
-      series: [{
-        name: 'city',
-        colorByPoint: true,
+      data: {
         data: [
           {name: 'Littleton', y: 2},
           {name: 'Denver', y: 2},
           {name: 'Aurora', y: 1},
           {name: 'Golden', y: 1}
-        ]
-      }],
-      title: {
-        text: 'Unique count of CITY'
+        ],
+        xAxis: [{
+          dataProperty: 'name'
+        }],
+        yAxis: [{
+          dataProperty: 'y',
+          label: 'Count'
+        }]
+      },
+      options: {
+        title: {
+          text: 'Unique count of CITY'
+        },
+        scales: {
+          xAxis: [{
+            scaleLabel: {
+              display: true
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true
+            }
+          }]
+        }
       }
     }
     
@@ -37,17 +56,8 @@ describe('barChart actions', () => {
 describe('barChart reducer', () => {
   it('should return the initial state', () => {
     const stateAfter = {
-      drilldown: {},
-      legend: {},
-      plotOptions: {},
-      series: [],
-      subtitle: {},
-      title: {
-        text: ''
-      },
-      tooltip: {},
-      xAxis: {},
-      yAxis: {}
+      data: {},
+      options: {}
     }
 
     expect(reducer(undefined, {})).toEqual(stateAfter)
@@ -55,21 +65,40 @@ describe('barChart reducer', () => {
 
   it('should handle SET', () => {
     const stateAfter = {
-      series: [{
-        name: 'city',
-        colorByPoint: true,
+      data: {
         data: [
           {name: 'Littleton', y: 2},
           {name: 'Denver', y: 2},
           {name: 'Aurora', y: 1},
           {name: 'Golden', y: 1}
-        ]
-      }],
-      title: {
-        text: 'Unique count of CITY'
+        ],
+        xAxis: [{
+          dataProperty: 'name'
+        }],
+        yAxis: [{
+          dataProperty: 'y',
+          label: 'Count'
+        }]
+      },
+      options: {
+        title: {
+          text: 'Unique count of CITY'
+        },
+        scales: {
+          xAxis: [{
+            scaleLabel: {
+              display: true
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true
+            }
+          }]
+        }
       }
     }
-    
+      
     const action = {
       type: SET,
       payload: {barChart: stateAfter}
